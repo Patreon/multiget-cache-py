9a2836bb23774253634ba3d01c648579d66f2ca6
Adam Bossy-Mendoza <adambossy@gmail.com>
2016-09-14 22:03:25 -0700

    Bump version to 0.0.9.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='multiget-cache',
-      version='0.0.8',
+      version='0.0.9',
       description='Python library for turning N function calls into 1 memoized call. Especially useful for SQL optimization.',
       url='http://github.com/Patreon/multiget-cache-py',
       author='Patreon',
